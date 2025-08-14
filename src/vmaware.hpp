e501bca6c680357dbfdbdfbec4c7065ba0ff2f59
李晓流 <2900629805@qq.com>
2025-08-15 02:24:02 +0800

    Update vmaware.hpp

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -2161,6 +2161,7 @@ private: // START OF PRIVATE VM DETECTION TECHNIQUE DEFINITIONS
     		if (util::hyper_x() == HYPERV_ARTIFACT_VM) {
                 return false;
             }
+			debug("HYPERVISOR_STR: strlen(out + 4)=", strlen(out + 4));
             return (std::strlen(out + 4) >= 4);
         #endif
     }
